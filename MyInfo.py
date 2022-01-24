c3050a575a61eb497a4f904ff17e93716c19b82e
pfrohn <91150762+pfrohn@users.noreply.github.com>
2022-01-24 18:56:04 -0500

    MyInfo Assignment Version 2

---
+++ MyInfo.py
@@ -0,0 +1,8 @@
+#CS175L-50
+#Patrick Frohn
+#myInfo
+
+print("My name is Patrick Frohn")
+print("I live at 2 Dreahook Road in Whitehouse Station, New Jersey, Zip: 08889")
+print("My phone number is 908-864-3043")
+print("My college major is Computer Science")
