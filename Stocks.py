29644b881e54b307ecb392f68fb3ce93d4b70e03
pfrohn <91150762+pfrohn@users.noreply.github.com>
2022-01-31 19:05:20 -0500

    Stock assignment 1/31/22

---
+++ Stocks.py
@@ -0,0 +1,22 @@
+COMMISSION_RATE = float (input("What is the commission rate? "))
+NUM_SHARES = int (input("How many shares do you have? "))
+PURCHASE_PRICE = float (input("What is the purchase price? "))
+SELLING_PRICE = float (input("What is the selling price? "))
+
+
+amountPaidForStock = NUM_SHARES * PURCHASE_PRICE
+purchaseCommission = amountPaidForStock * COMMISSION_RATE
+totalPaid = amountPaidForStock + purchaseCommission
+stockSoldFor = NUM_SHARES * SELLING_PRICE
+sellingCommission = COMMISSION_RATE * stockSoldFor
+totalRecieved = stockSoldFor - sellingCommission
+profitOrLoss = totalRecieved - totalPaid
+totalCommission = purchaseCommission + sellingCommission
+
+print("Amount paid for stock was $" + str(amountPaidForStock))
+print("Commission paid on the purchase is $" + str(purchaseCommission))
+print("Amount the stock sold for is $" + str(stockSoldFor))
+print("Commission paid on the sale is $" + str(sellingCommission))
+print("Total Commission paid: $" + str(totalCommission))
+print("Profit or Loss is $" + str(profitOrLoss))
+
